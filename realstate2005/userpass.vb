01856ace526f41450a570f2b66ece6a1997aabff
hagerhaj <hager39@yahoo.com>
2023-12-19 11:28:20 +0200

    RealStateEast

---
+++ realstate2005/userpass.vb
@@ -0,0 +1,26 @@
+﻿Public Class userpass
+
+    Private Sub Label1_Click(sender As System.Object, e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        If TextBox1.Text = "ag" And TextBox2.Text = "123" Then
+
+            MDIParent1.ShowDialog()
+            Me.Close()
+
+
+        Else : MsgBox("Sorry, The Username or Password was incorrect.", MsgBoxStyle.Critical, "Information")
+
+        End If
+
+    End Sub
+End Class
